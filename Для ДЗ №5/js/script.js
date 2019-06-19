dae8d2849bf60c174f21c2c979fa5216ffcc05aa
Kirill <bakugan111k@ya.ru>
2019-06-19 12:27:03 +0600

    Lession 5

---
+++ Для ДЗ №5/js/script.js
@@ -0,0 +1,18 @@
+let menuItem = document.querySelectorAll(".menu-item"),
+    menu = document.querySelector(".menu");
+menu.insertBefore(menuItem[2], menuItem[1]);
+
+let menuItem5 = document.createElement("li");
+menu.appendChild(menuItem5);
+menuItem5.classList.add("menu-item");
+menuItem5.textContent = "Пятый пункт";
+
+let body = document.querySelector("body");
+body.style.background = 'url(./img/apple_true.jpg) center no-repeat';
+
+document.getElementById("title").textContent = 'Мы продаем только подлинную технику Apple';
+
+document.querySelector(".adv").remove();
+
+let answer = prompt("Каково ваше отношение к технике Apple?");
+document.getElementById("prompt").textContent = answer;
